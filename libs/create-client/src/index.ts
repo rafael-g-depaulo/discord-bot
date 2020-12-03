9eddfab60aac1df3d923cdeae26f7bba7fffa5de
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-12-03 11:35:22 -0300

    🚧 Add empty type definitions

--- libs/create-client/src/index.ts
+++ libs/create-client/src/index.ts
@@ -1,2 +1,18 @@
+import Discord from "discord.js"
 
-export const add = (a: number, b: number) => a + b
+export interface ClientOptions {
+
+}
+export interface Client {
+
+}
+
+export type CreateClient = (options: ClientOptions) => Client
+
+
+const createClient: CreateClient = (options) => {
+
+  return {}
+}
+
+export default createClient
