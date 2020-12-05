0ded264727ae3e35b5b942babf93feb8678bbfac
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-12-05 18:41:16 -0300

    🍱 Update advantage/disadvantage words

--- libs/dice/src/advWords.ts
+++ libs/dice/src/advWords.ts
@@ -1,7 +1,32 @@
 export const advantageWords: string[] = [
   "adv",
+  "adva",
+  "advan",
+  "advant",
+  "advantage",
+  "advantadge",
+  "van",
+  "vant",
+  "vanta",
+  "vantag",
+  "vantagem",
+  "vantajem",
 ]
 
 export const disadvantageWords: string[] = [
+  "des",
   "dis",
+  "desv",
+  "disv",
+  "dvan",
+  "dvant",
+  "disad",
+  "disadv",
+  "disvant",
+  "desvant",
+  "disadvan",
+  "desvantagem",
+  "disvantagem",
+  "disadvantage",
+  "disadvantadge",
 ]
