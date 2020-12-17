cbbf390717c17bec6575b1409cca2298a1a4d302
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-12-17 00:16:19 -0300

    ♻️ Use dice-module in app-test instead of dice lib and manual commands

--- apps/app-test/src/index.ts
+++ apps/app-test/src/index.ts
@@ -1,27 +1,12 @@
 import createClient, { Client,  Message } from "@discord-bot/create-client"
-import { createDice, getDiceRoll, testDiceRoll, resultString } from "@discord-bot/dice"
 
 import testModule from "@discord-bot/test-module"
+import diceModule from "@discord-bot/dice-module"
 
 const token = process.env.DISCORD_BOT_TOKEN
 const bot: Client = createClient({ token })
 
 bot.addModule(testModule)
-
-bot.addCommand({
-  id: "roll_dice",
-  test: (msg) => {
-    if (msg.content[0] !== "!") return false
-    return testDiceRoll(msg.content)
-  },
-  execute: (msg: Message) => {
-    const diceOptions = getDiceRoll(msg.content)
-    const dice = createDice(diceOptions)
-    const rollResults = dice.detailedRoll()
-    const rollResultStr = resultString(rollResults)
-
-    msg.reply(rollResultStr)
-  },
-})
+bot.addModule(diceModule)
 
 export default bot
