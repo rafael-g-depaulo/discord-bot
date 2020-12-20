582b6b7c4bf35279ecad215ba3f384045e25318a
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-12-20 18:37:22 -0300

    Add fantasy-battle module to app-test

--- apps/app-test/src/index.ts
+++ apps/app-test/src/index.ts
@@ -2,10 +2,12 @@ import createClient, { Client } from "@discord-bot/create-client"
 
 import testModule from "@discord-bot/test-module"
 import diceModule from "@discord-bot/dice-module"
+import fantasyBattle from "@discord-bot/fantasy-battle"
 
 const token = process.env.DISCORD_BOT_TOKEN
 const bot: Client = createClient({ token })
 
+bot.addModule(fantasyBattle)
 bot.addModule(testModule)
 bot.addModule(diceModule)
 
