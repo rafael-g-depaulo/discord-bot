5f556e894b108d480dab20783d86d4f6b794e858
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-12-22 16:30:46 -0300

    ✨ Add attributes to player model

--- modules/fantasy-battle/src/Models/PlayerCharacter/index.test.ts
+++ modules/fantasy-battle/src/Models/PlayerCharacter/index.test.ts
@@ -1,17 +1,40 @@
 import { useDbConnection } from "Utils/mongoTest"
 import PcModel, { Pc } from "./index"
 
+const mockAttributes = () => ({
+  Agility    : { value: 0, bonus: 0 },
+  Fortitude  : { value: 0, bonus: 0 },
+  Might      : { value: 0, bonus: 0 },
+  Learning   : { value: 0, bonus: 0 },
+  Logic      : { value: 0, bonus: 0 },
+  Perception : { value: 0, bonus: 0 },
+  Will       : { value: 0, bonus: 0 },
+  Deception  : { value: 0, bonus: 0 },
+  Persuasion : { value: 0, bonus: 0 },
+  Presence   : { value: 0, bonus: 0 },
+  Alteration : { value: 0, bonus: 0 },
+  Creation   : { value: 0, bonus: 0 },
+  Energy     : { value: 0, bonus: 0 },
+  Entropy    : { value: 0, bonus: 0 },
+  Influence  : { value: 0, bonus: 0 },
+  Movement   : { value: 0, bonus: 0 },
+  Prescience : { value: 0, bonus: 0 },
+  Protection : { value: 0, bonus: 0 },
+})
+
 describe("PlayerCharacter Model", () => {
 
   useDbConnection("PlayerCharacter")
 
   const pcInfo: Pc = {
-    name: "character name"
+    name: "character name",
+    attributes: mockAttributes(),
   }
   describe("CRUD", () => {
     it("creates", async () => {
       const pc = new PcModel(pcInfo)
       const pcSaved = await pc.save()
+
       expect(pc.name).toBe(pcInfo.name)
       expect(pc._id).toStrictEqual(pcSaved?._id)
       expect(pc.name).toBe(pcSaved.name)
@@ -31,12 +54,19 @@ describe("PlayerCharacter Model", () => {
       await pc.save()
 
       const read1 = await PcModel.findById(pc._id)
+
       pc.name = "new name"
+      pc.attributes.Creation.value = 2
+      pc.attributes.Creation.bonus = -1
       await pc.save()
 
       const read2 = await PcModel.findById(pc._id)
       expect(read1?.name).toEqual("character name")
+      expect(read1?.attributes.Creation.value).toEqual(0)
+      expect(read1?.attributes.Creation.bonus).toEqual(0)
       expect(read2?.name).toEqual("new name")
+      expect(read2?.attributes.Creation.value).toEqual(2)
+      expect(read2?.attributes.Creation.bonus).toEqual(-1)
     })
     
     it("deletes", async () => {
--- modules/fantasy-battle/src/Models/PlayerCharacter/index.ts
+++ modules/fantasy-battle/src/Models/PlayerCharacter/index.ts
@@ -1,22 +1,59 @@
 import { Schema, model, Model, Document, Types } from "mongoose"
-import { Relation, SchemaFields } from "../helpers"
+import { AttributeNames } from "../../PlayerCharacter/Attribute"
+import { SchemaFields } from "../helpers"
 
 // PC interface for document creation
 // this contains the real data typings for the type, but with typescript types and not mongo ones
 // relations don't show up here
 export interface Pc {
-  name: string
+  name: string,
+  attributes: {
+    [key in AttributeNames]: {
+      value: number,
+      bonus: number,
+    }
+  },
 }
 
 // mongoDb schema to define data type
 // this uses mongo types, not tipical typescript types
 // relations show up here
 // interface SchemaFields extends Record<keyof AboveInterface, any> { [extraKey: string]: any }
+const Attribute = {
+  value: {
+    type: Number,
+    default: 0,
+  },
+  bonus: {
+    type: Number,
+    default: 0,
+  },
+}
 const PcSchemaFields: SchemaFields<Pc> = {
   name: {
     type: String,
     required: true,
   },
+  attributes: {
+    Agility    : Attribute,
+    Fortitude  : Attribute,
+    Might      : Attribute,
+    Learning   : Attribute,
+    Logic      : Attribute,
+    Perception : Attribute,
+    Will       : Attribute,
+    Deception  : Attribute,
+    Persuasion : Attribute,
+    Presence   : Attribute,
+    Alteration : Attribute,
+    Creation   : Attribute,
+    Energy     : Attribute,
+    Entropy    : Attribute,
+    Influence  : Attribute,
+    Movement   : Attribute,
+    Prescience : Attribute,
+    Protection : Attribute,
+  }
 }
 export const PcSchema = new Schema<Pc>(PcSchemaFields)
 
