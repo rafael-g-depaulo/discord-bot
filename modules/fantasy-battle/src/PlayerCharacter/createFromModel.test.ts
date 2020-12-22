394a498b7b409f464bb85707861a08262e80364d
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-12-22 15:50:55 -0300

    ✨ Add createFromModel

---
+++ modules/fantasy-battle/src/PlayerCharacter/createFromModel.test.ts
@@ -0,0 +1,57 @@
+import PcModel, { Pc } from "Models/PlayerCharacter"
+import createFromModel from "./createFromModel"
+
+describe('createFromModel', () => {
+  it(`parses the character name`, () => {
+    const pcProps: Pc = {
+      name: "Vex",
+    }
+    const pcDoc = new PcModel(pcProps)
+    const createdFromModel = createFromModel(pcDoc)
+
+    expect(createdFromModel.name).toBe("Vex")
+  })
+
+  it(`parses the attributes`, () => {
+    // TODO: fix attributes
+      const pcProps: Pc = {
+        name: "Vex",
+      }
+      const pcDoc = new PcModel(pcProps)
+      const character = createFromModel(pcDoc)
+      expect(character.attributes).toMatchObject({
+        Agility:    { bonus: 0, value: 0 }, Fortitude:  { bonus: 0, value: 0 },
+        Might:      { bonus: 0, value: 0 }, Learning:   { bonus: 0, value: 0 },
+        Logic:      { bonus: 0, value: 0 }, Perception: { bonus: 0, value: 0 },
+        Will:       { bonus: 0, value: 0 }, Deception:  { bonus: 0, value: 0 },
+        Persuasion: { bonus: 0, value: 0 }, Presence:   { bonus: 0, value: 0 },
+        Alteration: { bonus: 0, value: 0 }, Creation:   { bonus: 0, value: 0 },
+        Energy:     { bonus: 0, value: 0 }, Entropy:    { bonus: 0, value: 0 },
+        Influence:  { bonus: 0, value: 0 }, Movement:   { bonus: 0, value: 0 },
+        Prescience: { bonus: 0, value: 0 }, Protection: { bonus: 0, value: 0 },
+      })
+  })
+
+  it(`has functioning attributes`, () => {
+    const pcProps: Pc = {
+      name: "Vex",
+    }
+    const pcDoc = new PcModel(pcProps)
+    const char = createFromModel(pcDoc)
+
+    const agility0SkillRoll = char.attributes.Agility.rollAttribute()
+    const agility0DmgRoll = char.attributes.Agility.rollDmg()
+
+    char.attributes.Agility.bonus = 4
+    char.attributes.Agility.value = 1
+
+    const agility3SkillRoll = char.attributes.Agility.rollAttribute()
+    const agility3DmgRoll = char.attributes.Agility.rollDmg()
+
+    expect(agility0SkillRoll.diceArgs).toEqual(expect.objectContaining({ dieMax: 20, dieAmmount: 1, bonus: 0 }))
+    expect(agility0DmgRoll.diceArgs).toEqual(expect.objectContaining({ dieMax: 2, dieAmmount: 1, bonus: 0 }))
+    
+    expect(agility3SkillRoll.diceArgs).toEqual(expect.objectContaining({ dieMax: 20, dieAmmount: 1, bonus: 10 }))
+    expect(agility3DmgRoll.diceArgs).toEqual(expect.objectContaining({ dieMax: 6, dieAmmount: 2, bonus: 0 }))
+  })
+})
---
+++ modules/fantasy-battle/src/PlayerCharacter/createFromModel.ts
@@ -0,0 +1,33 @@
+import { PcDocument } from "Models/PlayerCharacter"
+import { createAttributes } from "./Attribute"
+import PlayerCharacter, { PlayerCharacterState } from "./PlayerCharacter"
+import { saveFactory } from "./save"
+
+const createFromModel = (model: PcDocument): PlayerCharacter => {
+  
+  // player state
+  const state: PlayerCharacterState = {
+    name: model.name,
+    attributes: createAttributes(),
+    model,
+  }
+
+  // methods
+  const save = saveFactory(state)
+  
+  return {
+    // name prop
+    get name() { return state.name },
+    set name(v) { state.name = v },
+
+    // attributes
+    get attributes() { return state.attributes },
+
+    // model
+    get model() { return state.model },
+
+    save,
+  }
+}
+
+export default createFromModel
