4ebbc9ad48f3834c1f04517131317e5ae21c8fdb
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-12-22 22:29:49 -0300

    ✨ Save PlayerCharacter attributes on save

--- modules/fantasy-battle/src/PlayerCharacter/save.test.ts
+++ modules/fantasy-battle/src/PlayerCharacter/save.test.ts
@@ -8,32 +8,33 @@ import { saveFactory } from "./save"
 describe("PlayerCharacter.save()", () => {
   useDbConnection("PlayerCharacter_save")
 
-  const mockState = (props: Pc): PlayerCharacterState => ({
+  const mockAttributes = () => ({
+    Agility:    { value: 0, bonus: 0 } as Attribute,
+    Alteration: { value: 0, bonus: 0 } as Attribute,
+    Creation:   { value: 0, bonus: 0 } as Attribute,
+    Deception:  { value: 0, bonus: 0 } as Attribute,
+    Energy:     { value: 0, bonus: 0 } as Attribute,
+    Entropy:    { value: 0, bonus: 0 } as Attribute,
+    Fortitude:  { value: 0, bonus: 0 } as Attribute,
+    Influence:  { value: 0, bonus: 0 } as Attribute,
+    Learning:   { value: 0, bonus: 0 } as Attribute,
+    Logic:      { value: 0, bonus: 0 } as Attribute,
+    Might:      { value: 0, bonus: 0 } as Attribute,
+    Movement:   { value: 0, bonus: 0 } as Attribute,
+    Perception: { value: 0, bonus: 0 } as Attribute,
+    Persuasion: { value: 0, bonus: 0 } as Attribute,
+    Prescience: { value: 0, bonus: 0 } as Attribute,
+    Presence:   { value: 0, bonus: 0 } as Attribute,
+    Protection: { value: 0, bonus: 0 } as Attribute,
+    Will:       { value: 0, bonus: 0 } as Attribute,
+  })
+  const mockState = (props: Omit<Pc, "attributes">): PlayerCharacterState => ({
     name: props.name,
-    model: new PcModel(props),
-    attributes: {
-      Agility:    { value: 0, bonus: 0 } as Attribute,
-      Alteration: { value: 0, bonus: 0 } as Attribute,
-      Creation:   { value: 0, bonus: 0 } as Attribute,
-      Deception:  { value: 0, bonus: 0 } as Attribute,
-      Energy:     { value: 0, bonus: 0 } as Attribute,
-      Entropy:    { value: 0, bonus: 0 } as Attribute,
-      Fortitude:  { value: 0, bonus: 0 } as Attribute,
-      Influence:  { value: 0, bonus: 0 } as Attribute,
-      Learning:   { value: 0, bonus: 0 } as Attribute,
-      Logic:      { value: 0, bonus: 0 } as Attribute,
-      Might:      { value: 0, bonus: 0 } as Attribute,
-      Movement:   { value: 0, bonus: 0 } as Attribute,
-      Perception: { value: 0, bonus: 0 } as Attribute,
-      Persuasion: { value: 0, bonus: 0 } as Attribute,
-      Prescience: { value: 0, bonus: 0 } as Attribute,
-      Presence:   { value: 0, bonus: 0 } as Attribute,
-      Protection: { value: 0, bonus: 0 } as Attribute,
-      Will:       { value: 0, bonus: 0 } as Attribute,
-    }
+    model: new PcModel({ attributes: mockAttributes(), ...props }),
+    attributes: mockAttributes()
   })
 
-  it('works', async () => {
+  it(`saves the character's name`, async () => {
     const state = mockState({ name: "currentName" })
     const save = saveFactory(state)
 
@@ -44,5 +45,32 @@ describe("PlayerCharacter.save()", () => {
     await save()
     const saved2 = await PcModel.findById(state.model._id)
     expect(saved2?.name).toBe("new name")
+  })
+
+  it(`saves the character's attributes`, async () => {
+    const state = mockState({ name: "currentName" })
+    state.attributes.Agility.value = 2
+    state.attributes.Agility.bonus = 1
+
+    const save = saveFactory(state)
+
+    const saved1 = await save()
+    expect(saved1.name).toBe("currentName")
+    expect(saved1.attributes.Agility.value).toBe(2)
+    expect(saved1.attributes.Agility.bonus).toBe(1)
+
+    state.name = "new name"
+    state.attributes.Agility.value = 3
+    state.attributes.Agility.bonus = 2
+    state.attributes.Might.value = 1
+    state.attributes.Deception.bonus = 2
+
+    await save()
+    const saved2 = await PcModel.findById(state.model._id)
+    expect(saved2?.name).toBe("new name")
+    expect(saved2?.attributes.Agility.value).toBe(3)
+    expect(saved2?.attributes.Agility.bonus).toBe(2)
+    expect(saved2?.attributes.Might.value).toBe(1)
+    expect(saved2?.attributes.Deception.bonus).toBe(2)
   })
 })
--- modules/fantasy-battle/src/PlayerCharacter/save.ts
+++ modules/fantasy-battle/src/PlayerCharacter/save.ts
@@ -1,3 +1,4 @@
+import { AttributeNames, AttributeProperties } from "./Attribute"
 import { PlayerCharacterState } from "./PlayerCharacter"
 
 export const saveFactory = (state: PlayerCharacterState) => () => {
@@ -5,10 +6,30 @@ export const saveFactory = (state: PlayerCharacterState) => () => {
   const {
     name,
     model,
+    attributes,
   } = state
 
   // update model with current state
   model.name = name
+  // update attributes
+  model.attributes.Agility    = attributes.Agility
+  model.attributes.Fortitude  = attributes.Fortitude
+  model.attributes.Might      = attributes.Might
+  model.attributes.Learning   = attributes.Learning
+  model.attributes.Logic      = attributes.Logic
+  model.attributes.Perception = attributes.Perception
+  model.attributes.Will       = attributes.Will
+  model.attributes.Deception  = attributes.Deception
+  model.attributes.Persuasion = attributes.Persuasion
+  model.attributes.Presence   = attributes.Presence
+  model.attributes.Alteration = attributes.Alteration
+  model.attributes.Creation   = attributes.Creation
+  model.attributes.Energy     = attributes.Energy
+  model.attributes.Entropy    = attributes.Entropy
+  model.attributes.Influence  = attributes.Influence
+  model.attributes.Movement   = attributes.Movement
+  model.attributes.Prescience = attributes.Prescience
+  model.attributes.Protection = attributes.Protection
 
   // save
   return model.save()
