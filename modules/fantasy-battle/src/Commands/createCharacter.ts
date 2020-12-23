e77a9a3314e6dacf7241846a91564b832d943510
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-12-23 12:21:19 -0300

    ✨ Finish createCharacter Command

--- modules/fantasy-battle/src/Commands/createCharacter.ts
+++ modules/fantasy-battle/src/Commands/createCharacter.ts
@@ -1,37 +1,55 @@
 import { Command, Message } from "@discord-bot/create-client"
 
+import { parseArgsStringToArgv } from "string-argv"
+import yargs from "yargs"
+
 import logger from "../Utils/logger"
 import { isDm, isPlayer } from "../Utils/userPermissions"
 import { getOrCreatePlayerUser } from "../Utils/getOrCreateUser"
 
+import { createCharacter as createPc } from "../PlayerCharacter"
+
 const createCharacter: Command = {
   id: "Fantasy Battle: createCharacter",
-  test: /!create-char/i,
-  execute: async (msg: Message) => {
+  test: /!create-char\s*(?<args>.*)?$/i,
+  execute: async (msg: Message, regexResult) => {
     // if user isn't admin or Player or DM, ignore
-    if (!isPlayer(msg.member) && !isDm(msg.member))
-    return msg.channel.send(`sorry, but only people with the "Player" or "Dm" role can use this command`)
+    if (!isPlayer(msg.member) && !isDm(msg.member)) {
+      logger.info(`FB: (Command) createCharacter: user "${msg.author.username}" tried to !create-char but isn't a Player or DM`)
+      return msg.channel.send(`sorry, but only people with the "Player" or "Dm" role can use this command`)
+    }
 
-    // if player is already saved in DB, retrieve it
-    console.log(msg.author.id, typeof msg.author.id)
-
+    // create of fetch the player user instance
     const user = await getOrCreatePlayerUser({ userId: msg.author.id, username: msg.author.username })
 
-    // logic to parse arguments and create character goes here
-    // logic to parse arguments and create character goes here
-    // logic to parse arguments and create character goes here
-    // logic to parse arguments and create character goes here
-    // logic to parse arguments and create character goes here
-    // logic to parse arguments and create character goes here
-    // logic to parse arguments and create character goes here
-    // logic to parse arguments and create character goes here
-    // logic to parse arguments and create character goes here
-    // logic to parse arguments and create character goes here
-    // logic to parse arguments and create character goes here
-    // logic to parse arguments and create character goes here
+    // parse arguments
+    const argsArr = parseArgsStringToArgv(regexResult?.groups?.args ?? "")
+    const args = yargs(argsArr).argv
 
-    // if player isn't already in DB, create it
-    msg.channel.send("")
+    // if bad arguments
+    if (!args.name || !(typeof args?.name === 'string') || args?.name === "") {
+      logger.info(`FB: (Command) createCharacter: user "${msg.author.username}" tried to !create-char without giving a character name`)
+      return msg.channel.send(`!create-char command needs argument --name="characterNameHere" or --name characterName`)
+    }
+
+    // extract args
+    const {
+      name,
+    } = args
+
+    // if repeated name, return
+    if (user.characters.some(char => char.name === name)) {
+      logger.info(`FB: (Command) createCharacter: user "${msg.author.username}" tried to !create-char, but already had a character named "${name}"`)
+      return msg.channel.send(`You already have a character named ${name}! You can't repeat names, be more creative`)
+    }
+
+    // create character
+    const newPc = createPc({ name: args.name })
+    await user.addCharacter(newPc)
+
+    logger.info(`FB: (Command) createCharacter: user "${msg.author.username}" created character ${newPc.name}`)
+    msg.channel.send(`Ok! Character "${newPc.name}" created for ${user.username}`)
+
     return
   },
 }
