076ff82de4ae4d8178c57166ee4651aa532e0043
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-12-28 23:42:58 -0300

    ♻️ Refactor fantasy-battle commands

--- modules/fantasy-battle/src/Commands/createCharacter.ts
+++ modules/fantasy-battle/src/Commands/createCharacter.ts
@@ -1,4 +1,5 @@
-import { Command, Message } from "@discord-bot/create-client"
+import { Command } from "@discord-bot/create-client"
+import { RegexCommand } from "@discord-bot/create-client"
 
 import { parseArgsStringToArgv } from "string-argv"
 import yargs from "yargs"
@@ -9,49 +10,53 @@ import { isDm, isPlayer } from "../Utils/userPermissions"
 import PlayerUserModel from "../Models/PlayerUser"
 import PcModel from "../Models/PlayerCharacter"
 
-const createCharacter: Command = {
+export const test: RegexCommand.test = /!create-char\s*(?<args>.*)?$/i
+
+export const execute: RegexCommand.execute = async (msg, regexResult) => {
+  // if user isn't admin or Player or DM, ignore
+  if (!isPlayer(msg.member) && !isDm(msg.member)) {
+    logger.info(`FB: (Command) createCharacter: user "${msg.author.username}" tried to !create-char but isn't a Player or DM`)
+    return msg.channel.send(`sorry, but only people with the "Player" or "Dm" role can use this command`)
+  }
+
+  // create of fetch the player user instance
+  const user = await PlayerUserModel.getOrCreate({ userId: msg.author.id, username: msg.author.username })
+
+  // parse arguments
+  const argsArr = parseArgsStringToArgv(regexResult?.groups?.args ?? "")
+  const args = yargs(argsArr).argv
+
+  // if bad arguments
+  if (!args.name || !(typeof args?.name === 'string') || args?.name === "") {
+    logger.info(`FB: (Command) createCharacter: user "${msg.author.username}" tried to !create-char without giving a character name`)
+    return msg.channel.send(`!create-char command needs argument --name="characterNameHere" or --name characterName`)
+  }
+
+  // extract args
+  const {
+    name,
+  } = args
+
+  // if repeated name, return
+  if (user.characters.some(char => char.name === name)) {
+    logger.info(`FB: (Command) createCharacter: user "${msg.author.username}" tried to !create-char, but already had a character named "${name}"`)
+    return msg.channel.send(`You already have a character named ${name}! You can't repeat names, be more creative`)
+  }
+
+  // create character
+  const newPc = PcModel.createCharacter({ name: args.name })
+  await user.addCharacter(newPc)
+
+  logger.info(`FB: (Command) createCharacter: user "${msg.author.username}" created character ${newPc.name}`)
+  msg.channel.send(`Ok! Character "${newPc.name}" created for ${user.username}`)
+
+  return
+}
+
+export const createCharacter: Command = {
   id: "Fantasy Battle: createCharacter",
-  test: /!create-char\s*(?<args>.*)?$/i,
-  execute: async (msg: Message, regexResult) => {
-    // if user isn't admin or Player or DM, ignore
-    if (!isPlayer(msg.member) && !isDm(msg.member)) {
-      logger.info(`FB: (Command) createCharacter: user "${msg.author.username}" tried to !create-char but isn't a Player or DM`)
-      return msg.channel.send(`sorry, but only people with the "Player" or "Dm" role can use this command`)
-    }
-
-    // create of fetch the player user instance
-    const user = await PlayerUserModel.getOrCreate({ userId: msg.author.id, username: msg.author.username })
-
-    // parse arguments
-    const argsArr = parseArgsStringToArgv(regexResult?.groups?.args ?? "")
-    const args = yargs(argsArr).argv
-
-    // if bad arguments
-    if (!args.name || !(typeof args?.name === 'string') || args?.name === "") {
-      logger.info(`FB: (Command) createCharacter: user "${msg.author.username}" tried to !create-char without giving a character name`)
-      return msg.channel.send(`!create-char command needs argument --name="characterNameHere" or --name characterName`)
-    }
-
-    // extract args
-    const {
-      name,
-    } = args
-
-    // if repeated name, return
-    if (user.characters.some(char => char.name === name)) {
-      logger.info(`FB: (Command) createCharacter: user "${msg.author.username}" tried to !create-char, but already had a character named "${name}"`)
-      return msg.channel.send(`You already have a character named ${name}! You can't repeat names, be more creative`)
-    }
-
-    // create character
-    const newPc = PcModel.createCharacter({ name: args.name })
-    await user.addCharacter(newPc)
-
-    logger.info(`FB: (Command) createCharacter: user "${msg.author.username}" created character ${newPc.name}`)
-    msg.channel.send(`Ok! Character "${newPc.name}" created for ${user.username}`)
-
-    return
-  },
+  test,
+  execute,
 }
 
 export default createCharacter
--- modules/fantasy-battle/src/Commands/createRoles.ts
+++ modules/fantasy-battle/src/Commands/createRoles.ts
@@ -1,54 +1,58 @@
-import { Command, Message } from "@discord-bot/create-client";
+import { Command, RegexCommand } from "@discord-bot/create-client"
+
+export const test: RegexCommand.test = /!create-roles/i
+
+export const execute: RegexCommand.execute = async (msg) => {
+
+  // if user who used command isn't admin or DM, ignore
+  const userRoles = msg.member?.roles.cache
+  const isAdmin = msg.member?.hasPermission("ADMINISTRATOR")
+  if (!userRoles?.some(role => role.name === "DM") && !isAdmin)
+    return msg.channel.send(`sorry, but only DM's or server admins can use the "!create-roles" command`)
+
+  // get server roles
+  const roles = await msg.guild?.roles.fetch()
+  const roleNames = roles?.cache.array().map(role => role.name)
+
+  // check Player and dm roles
+  const hasPlayerRole = roleNames?.includes("Player")
+  const hasDmRole = roleNames?.includes("DM")
+
+  // if both roles exist, exit
+  if (hasPlayerRole && hasDmRole) return msg.channel.send(`both roles  ("DM" and "Player") already exist, everything's ok!`)
+
+  // if "Player" role doesn't exist, create it
+  if (!hasPlayerRole) {
+    roles?.create({
+      data: {
+        name: "Player",
+        mentionable: true,
+        color: "#e0ba79"
+      },
+      reason: "role for players of OL: Fantasy Battle RPG (role created by Dice-roller Bot, made by Ragan)",
+    })
+    msg.channel.send(`this server didn't have the "Player" role, so i created it`)
+  }
+
+  // if "DM" role doesn't exist, create it
+  if (!hasDmRole) {
+    roles?.create({
+      data: {
+        name: "DM",
+        mentionable: true,
+        color: "#3fbef3"
+      },
+      reason: "role for DM's of OL: Fantasy Battle RPG (role created by Dice-roller Bot, made by Ragan)",
+    })
+    msg.channel.send(`this server didn't have "DM" role, so i created it`)
+  }
+  return
+}
 
 const createRoles: Command = {
   id: "Fantasy Battle: createRoles",
-  test: /!create-roles/i,
-  execute: async (msg: Message) => {
-
-    // if user who used command isn't admin or DM, ignore
-    const userRoles = msg.member?.roles.cache
-    const isAdmin = msg.member?.hasPermission("ADMINISTRATOR")
-    if (!userRoles?.some(role => role.name === "DM") && !isAdmin)
-      return msg.channel.send(`sorry, but only DM's or server admins can use the "!create-roles" command`)
-
-    // get server roles
-    const roles = await msg.guild?.roles.fetch()
-    const roleNames = roles?.cache.array().map(role => role.name)
-
-    // check Player and dm roles
-    const hasPlayerRole = roleNames?.includes("Player")
-    const hasDmRole = roleNames?.includes("DM")
-
-    // if both roles exist, exit
-    if (hasPlayerRole && hasDmRole) return msg.channel.send(`both roles  ("DM" and "Player") already exist, everything's ok!`)
-
-    // if "Player" role doesn't exist, create it
-    if (!hasPlayerRole) {
-      roles?.create({
-        data: {
-          name: "Player",
-          mentionable: true,
-          color: "#e0ba79"
-        },
-        reason: "role for players of OL: Fantasy Battle RPG (role created by Dice-roller Bot, made by Ragan)",
-      })
-      msg.channel.send(`this server didn't have the "Player" role, so i created it`)
-    }
-
-    // if "DM" role doesn't exist, create it
-    if (!hasDmRole) {
-      roles?.create({
-        data: {
-          name: "DM",
-          mentionable: true,
-          color: "#3fbef3"
-        },
-        reason: "role for DM's of OL: Fantasy Battle RPG (role created by Dice-roller Bot, made by Ragan)",
-      })
-      msg.channel.send(`this server didn't have "DM" role, so i created it`)
-    }
-    return
-  },
+  test,
+  execute,
 }
 
 export default createRoles
