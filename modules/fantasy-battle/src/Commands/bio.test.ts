bb81e87820c1da433dd81b6187d739ff68438aa4
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-01-09 12:12:39 -0300

    ✨ Add bio command

---
+++ modules/fantasy-battle/src/Commands/bio.test.ts
@@ -0,0 +1,221 @@
+import { mockMessage } from "@discord-bot/discord-mock"
+import PcModel from "Models/PlayerCharacter"
+import PlayerUserModel from "Models/PlayerUser"
+import { mockDmMessage, mockPlayerMessage } from "Utils/mockMessage"
+import { useDbConnection } from "Utils/mongoTest"
+
+import { test, execute } from "./bio"
+
+describe("Command: rollDmg", () => {
+  useDbConnection("Command_rollDmg")
+
+  describe(".test", () => {
+    it("works", () => {
+      expect(`!bio`).toMatch(test)
+      expect(`!wrong`).not.toMatch(test)
+    })
+    it(`captures flags`, () => {
+      expect(`!bio --character Kuff`).toMatch(test)
+      expect(test.exec(`!bio --character Kuff`)?.groups).toMatchObject({ flags: "--character Kuff" })
+    })
+  })
+
+  describe(".execute", () => {
+    describe("permissions", () => {
+      it(`doesn't allow a non-player, non-DM user to use command`, async () => {
+        const [ message ] = mockMessage()
+        message.content = `!bio`
+        await execute(message, test.exec(message.content)!)
+        expect(message.channel.send).toBeCalledTimes(1)
+        expect(message.channel.send).toBeCalledWith(`sorry, but only people with the \"Player\" or \"Dm\" role can use this command`)
+      })
+      
+      it(`doesn't allow a non-DM user to use command with --player flag`, async () => {
+        const [message] = mockPlayerMessage()
+        message.content = `!bio --player "OtherPlayer"`
+        await execute(message, test.exec(message.content)!)
+        expect(message.channel.send).toBeCalledTimes(1)
+        expect(message.channel.send).toBeCalledWith(`only DM's can use this command with the --player flag`)
+      })
+    })
+    
+    describe("dealing with bad args", () => {
+      it(`complains if --char doesn't exist`, async () => {
+        const [message] = mockPlayerMessage()
+        message.content = `!bio --char Kuff`
+        await execute(message, test.exec(message.content)!)
+        expect(message.channel.send).toBeCalledTimes(1)
+        expect(message.channel.send).toBeCalledWith(`Player ${message.author.username} doesn't have a character that matches "Kuff". Try "!listChars" to see available characters`)
+      })
+
+      it(`complains if --player doesn't exist`, async () => {
+        const [message] = mockDmMessage()
+        message.content = `!bio --player "Ragan" --char "Kuff"`
+        await execute(message, test.exec(message.content)!)
+        expect(message.channel.send).toBeCalledTimes(1)
+        expect(message.channel.send).toBeCalledWith(`player "Ragan" doesn't exist in my database. are you sure you typed their name correctly?`)
+      })
+    })
+    
+    describe("happy paths", () => {
+      it("works by default", async () => {
+        const [message] = mockPlayerMessage()
+        message.content = `!bio`
+
+        const playerUser = PlayerUserModel.createUser({ userId: message.author.id, username: message.author.username })
+        playerUser.addCharacter(PcModel.createCharacter({ name: "Horu", level: 3 }))
+        playerUser.characters[0].attributes.Might.value = 4
+        playerUser.characters[0].attributes.Might.bonus = -3
+        playerUser.characters[0].attributes.Creation.bonus = 3
+        playerUser.characters[0].updateMaxResources()
+        playerUser.characters[0].hp.current = 11
+        playerUser.characters[0].mp.current = 8
+
+        await playerUser.save()
+        await execute(message, test.exec(message.content)!)
+
+        expect(message.channel.send).toBeCalledTimes(1)
+        const messageSent = (message.channel.send as unknown as jest.MockedFunction<(a:string) => {}>).mock.calls[0][0]
+        
+        expect(messageSent).toBe(
+          "**Horu** (level 3)\n" +
+          "\nHP:   **11/20**" +
+          "\nMP:   **8/14**" +
+
+          "\n\n**Physical**:" +
+          "\n\tAgility: 0" + 
+          "\n\tFortitude: 0" + 
+          "\n\tMight: 4 (-3)" + 
+
+          "\n\n**Mental**:" +
+          "\n\tLearning: 0" +
+          "\n\tLogic: 0" +
+          "\n\tPerception: 0" +
+          "\n\tWill: 0" +
+          
+          "\n\n**Social**:" +
+          "\n\tDeception: 0" +
+          "\n\tPersuasion: 0" +
+          "\n\tPresence: 0" +
+          
+          "\n\n**Special**:" +
+          "\n\tAlteration: 0" +
+          "\n\tCreation: 0 (+3)" +
+          "\n\tEnergy: 0" +
+          "\n\tEntropy: 0" +
+          "\n\tInfluence: 0" +
+          "\n\tMovement: 0" +
+          "\n\tPrescience: 0" +
+          "\n\tProtection: 0"
+        )
+      })
+
+      it("works with --char flag", async () => {
+        const [message] = mockPlayerMessage()
+        message.content = `!bio --char Kuff`
+
+        const playerUser = PlayerUserModel.createUser({ userId: message.author.id, username: message.author.username })
+        playerUser.addCharacter(PcModel.createCharacter({ name: "Horu" }))
+        playerUser.addCharacter(PcModel.createCharacter({ name: "Kuff" }))
+        playerUser.characters[0].attributes.Fortitude.value = 3 // horu
+        playerUser.characters[1].attributes.Fortitude.value = 8 // kuff
+        playerUser.characters[1].attributes.Might.value = 4
+        playerUser.characters[1].attributes.Might.bonus = -3
+        playerUser.characters[1].attributes.Creation.bonus = 3
+        playerUser.characters[1].updateMaxResources()
+        playerUser.characters[1].hp.current = 11
+        playerUser.characters[1].mp.current = 8
+        await playerUser.save()
+
+        await execute(message, test.exec(message.content)!)
+
+        expect(message.channel.send).toBeCalledTimes(1)
+        const messageSent = (message.channel.send as unknown as jest.MockedFunction<(a:string) => {}>).mock.calls[0][0]
+        
+        expect(messageSent).toBe(
+          "**Kuff** (level 1)\n" +
+          "\nHP:   **11/32**" +
+          "\nMP:   **8/10**" +
+
+          "\n\n**Physical**:" +
+          "\n\tAgility: 0" + 
+          "\n\tFortitude: 8" + 
+          "\n\tMight: 4 (-3)" + 
+
+          "\n\n**Mental**:" +
+          "\n\tLearning: 0" +
+          "\n\tLogic: 0" +
+          "\n\tPerception: 0" +
+          "\n\tWill: 0" +
+          
+          "\n\n**Social**:" +
+          "\n\tDeception: 0" +
+          "\n\tPersuasion: 0" +
+          "\n\tPresence: 0" +
+          
+          "\n\n**Special**:" +
+          "\n\tAlteration: 0" +
+          "\n\tCreation: 0 (+3)" +
+          "\n\tEnergy: 0" +
+          "\n\tEntropy: 0" +
+          "\n\tInfluence: 0" +
+          "\n\tMovement: 0" +
+          "\n\tPrescience: 0" +
+          "\n\tProtection: 0"
+        )
+      })
+      it(`works with --player flag`, async () => {
+        const [message] = mockDmMessage()
+        message.content = `!bio --player "testPlayer"`
+
+        const playerUser = PlayerUserModel.createUser({ userId: "42069420", username: "testPlayer" })
+        playerUser.addCharacter(PcModel.createCharacter({ name: "Llat Drat'sab", level: 2 }))
+        playerUser.characters[0].attributes.Fortitude.value = 3 // horu
+        playerUser.characters[0].attributes.Fortitude.value = 8 // kuff
+        playerUser.characters[0].attributes.Might.value = 4
+        playerUser.characters[0].attributes.Might.bonus = -3
+        playerUser.characters[0].attributes.Creation.bonus = 3
+        playerUser.characters[0].updateMaxResources()
+        playerUser.characters[0].hp.current = 11
+        playerUser.characters[0].mp.current = 8
+        await playerUser.save()
+
+        await execute(message, test.exec(message.content)!)
+
+        expect(message.channel.send).toBeCalledTimes(1)
+        const messageSent = (message.channel.send as unknown as jest.MockedFunction<(a:string) => {}>).mock.calls[0][0]
+        expect(messageSent).toBe(
+          "**Llat Drat'sab** (level 2)\n" +
+          "\nHP:   **11/34**" +
+          "\nMP:   **8/12**" +
+
+          "\n\n**Physical**:" +
+          "\n\tAgility: 0" + 
+          "\n\tFortitude: 8" + 
+          "\n\tMight: 4 (-3)" + 
+
+          "\n\n**Mental**:" +
+          "\n\tLearning: 0" +
+          "\n\tLogic: 0" +
+          "\n\tPerception: 0" +
+          "\n\tWill: 0" +
+          
+          "\n\n**Social**:" +
+          "\n\tDeception: 0" +
+          "\n\tPersuasion: 0" +
+          "\n\tPresence: 0" +
+          
+          "\n\n**Special**:" +
+          "\n\tAlteration: 0" +
+          "\n\tCreation: 0 (+3)" +
+          "\n\tEnergy: 0" +
+          "\n\tEntropy: 0" +
+          "\n\tInfluence: 0" +
+          "\n\tMovement: 0" +
+          "\n\tPrescience: 0" +
+          "\n\tProtection: 0"
+        )
+      })
+    })
+  })
+})
---
+++ modules/fantasy-battle/src/Commands/bio.ts
@@ -0,0 +1,88 @@
+import { Command, RegexCommand } from "@discord-bot/create-client"
+import { ResourceDocument } from "Models/PcResource"
+
+import { Attribute } from "Models/PlayerCharacter/types"
+import { logSuccess } from "Utils/commandLog"
+
+import getPlayerChar from "Utils/getPlayerChar"
+import { getPlayerUser } from "Utils/getUser"
+import parseFlags, { FlagsObject } from "Utils/parseArgs"
+import rejectIfNotPlayerOrDm from "Utils/rejectIfNotPlayerOrDm"
+import { commandWithFlags } from "../Utils/regex"
+
+export const test: RegexCommand.test = commandWithFlags(
+  /bio/,
+)
+
+const bonusString = (bonus: number) => 
+  bonus > 0 ? ` (+${bonus})` :
+  bonus < 0 ? ` (${bonus})` :
+  ""
+
+const resourceString = (name: string, resource: ResourceDocument) =>
+  `\n${name}:   **${resource.current}/${resource.max}**`
+
+const attributeGroupString = (name: string) => `\n\n**${name}**:`
+const attributeString = (attbName: string, attb: Attribute) =>
+  `\n\t${attbName}: ${attb.value}${bonusString(attb.bonus)}`
+
+export const execute: RegexCommand.execute = async (message, regexResult) => {
+  
+  // if user isn't admin or Player or DM, ignore
+  if (rejectIfNotPlayerOrDm(message)) return
+
+  // parse arguments
+  const flagsObject: FlagsObject<{ player: string, char: string }> = {
+    player: { type: "string", optional: true },
+    char: { type: "string", optional: true },
+  }
+  const flags = parseFlags("!bio", flagsObject, regexResult?.groups?.flags, message)
+  if (flags === null) return
+  
+  const { player } = await getPlayerUser("!bio", message, flags.player)
+  if (player === null) return
+
+  const character = getPlayerChar("!bio", player, message, flags)
+  if (!character) return
+
+  const bioString = `**${character.name}** (level ${character.level})\n`
+    + resourceString("HP", character.hp)
+    + resourceString("MP", character.mp)
+
+    + attributeGroupString("Physical")
+    + attributeString("Agility"    , character.attributes.Agility)
+    + attributeString("Fortitude"  , character.attributes.Fortitude)
+    + attributeString("Might"      , character.attributes.Might)
+
+    + attributeGroupString("Mental")
+    + attributeString("Learning"   , character.attributes.Learning)
+    + attributeString("Logic"      , character.attributes.Logic)
+    + attributeString("Perception" , character.attributes.Perception)
+    + attributeString("Will"       , character.attributes.Will)
+
+    + attributeGroupString("Social")
+    + attributeString("Deception"  , character.attributes.Deception)
+    + attributeString("Persuasion" , character.attributes.Persuasion)
+    + attributeString("Presence"   , character.attributes.Presence)
+
+    + attributeGroupString("Special")
+    + attributeString("Alteration" , character.attributes.Alteration)
+    + attributeString("Creation"   , character.attributes.Creation)
+    + attributeString("Energy"     , character.attributes.Energy)
+    + attributeString("Entropy"    , character.attributes.Entropy)
+    + attributeString("Influence"  , character.attributes.Influence)
+    + attributeString("Movement"   , character.attributes.Movement)
+    + attributeString("Prescience" , character.attributes.Prescience)
+    + attributeString("Protection" , character.attributes.Protection)
+
+  logSuccess("!bio", message, flags)
+  message.channel.send(bioString)
+}
+
+export const bio: Command = {
+  id: "Fantasy Battle: bio",
+  test,
+  execute,
+}
+
+export default bio
