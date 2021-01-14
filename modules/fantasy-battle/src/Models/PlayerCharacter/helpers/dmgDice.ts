5d728ff3c3bdd3636b41af8832238f1f0bf1d3a3
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-01-14 14:35:04 -0300

    ♻️ Add explode to damageDice

--- modules/fantasy-battle/src/Models/PlayerCharacter/helpers/dmgDice.ts
+++ modules/fantasy-battle/src/Models/PlayerCharacter/helpers/dmgDice.ts
@@ -1,31 +1,31 @@
 import { DiceProps } from "@discord-bot/dice"
 
 export const damageDice: DiceProps[] = [
-  { dieAmmount:  1, dieMax:  2 },   // ( 0)  1d2
+  { dieAmmount:  1, dieMax:  2, explode: 1 },   // ( 0)  1d2!
   
-  { dieAmmount:  1, dieMax:  4 },   // ( 1)  1d4
-  { dieAmmount:  1, dieMax:  6 },   // ( 2)  1d6
-  { dieAmmount:  1, dieMax:  8 },   // ( 3)  1d8
-  { dieAmmount:  1, dieMax: 10 },   // ( 4)  1d10
-  { dieAmmount:  2, dieMax:  6 },   // ( 5)  2d6
+  { dieAmmount:  1, dieMax:  4, explode: 1 },   // ( 1)  1d4!
+  { dieAmmount:  1, dieMax:  6, explode: 1 },   // ( 2)  1d6!
+  { dieAmmount:  1, dieMax:  8, explode: 1 },   // ( 3)  1d8!
+  { dieAmmount:  1, dieMax: 10, explode: 1 },   // ( 4)  1d10!
+  { dieAmmount:  2, dieMax:  6, explode: 1 },   // ( 5)  2d6!
 
-  { dieAmmount:  2, dieMax:  8 },   // ( 6)  2d8
-  { dieAmmount:  2, dieMax: 10 },   // ( 7)  2d10
-  { dieAmmount:  3, dieMax:  8 },   // ( 8)  3d8
-  { dieAmmount:  3, dieMax: 10 },   // ( 9)  3d10
-  { dieAmmount:  4, dieMax: 10 },   // (10)  4d10
+  { dieAmmount:  2, dieMax:  8, explode: 1 },   // ( 6)  2d8!
+  { dieAmmount:  2, dieMax: 10, explode: 1 },   // ( 7)  2d10!
+  { dieAmmount:  3, dieMax:  8, explode: 1 },   // ( 8)  3d8!
+  { dieAmmount:  3, dieMax: 10, explode: 1 },   // ( 9)  3d10!
+  { dieAmmount:  4, dieMax: 10, explode: 1 },   // (10)  4d10!
   
-  { dieAmmount:  8, dieMax:  4 },   // (11)  8d4
-  { dieAmmount:  5, dieMax:  8 },   // (12)  5d8
-  { dieAmmount: 10, dieMax:  4 },   // (13) 10d4
-  { dieAmmount:  8, dieMax:  6 },   // (14)  8d6
-  { dieAmmount:  3, dieMax: 20 },   // (15)  3d20
+  { dieAmmount:  8, dieMax:  4, explode: 1 },   // (11)  8d4!
+  { dieAmmount:  5, dieMax:  8, explode: 1 },   // (12)  5d8!
+  { dieAmmount: 10, dieMax:  4, explode: 1 },   // (13) 10d4!
+  { dieAmmount:  8, dieMax:  6, explode: 1 },   // (14)  8d6!
+  { dieAmmount:  3, dieMax: 20, explode: 1 },   // (15)  3d20!
   
-  { dieAmmount: 10, dieMax:  6 },   // (16) 10d6
-  { dieAmmount:  6, dieMax: 12 },   // (17)  6d12
-  { dieAmmount:  8, dieMax: 10 },   // (18)  8d10
-  { dieAmmount: 11, dieMax:  8 },   // (19) 11d8
-  { dieAmmount: 10, dieMax: 10 },   // (20) 10d10
+  { dieAmmount: 10, dieMax:  6, explode: 1 },   // (16) 10d6!
+  { dieAmmount:  6, dieMax: 12, explode: 1 },   // (17)  6d12!
+  { dieAmmount:  8, dieMax: 10, explode: 1 },   // (18)  8d10!
+  { dieAmmount: 11, dieMax:  8, explode: 1 },   // (19) 11d8!
+  { dieAmmount: 10, dieMax: 10, explode: 1 },   // (20) 10d10!
 ]
 
 // get correct dice to roll for attribute value
--- modules/fantasy-battle/src/Models/PlayerCharacter/methods/rollDmg.ts
+++ modules/fantasy-battle/src/Models/PlayerCharacter/methods/rollDmg.ts
@@ -18,6 +18,8 @@ const useCache = (key: string, diceGenerator: () => Dice) => {
 const getAttributeDice = (dmgValue: number, args: DiceExtraArgs) => {
   // create diceArgs and add user overrides (advantage, bonus, explosion)
   const diceArgs: DiceProps = { ...getDmgDiceArg(dmgValue), ...args }
+  // override args if explosion = 0 (i.e.: if didn't specify exploion in args, use the default)
+  if (args.explode === 0) diceArgs.explode = getDmgDiceArg(dmgValue).explode
   return useCache(JSON.stringify(diceArgs), () => createDice(diceArgs))
 }
 
@@ -25,9 +27,8 @@ export const rollDmg: PcInstanceMethod<rollDmg> = function(this, attbName, rollA
   const {
     bonus = 0,
     advantage = 0,
+    explode
   } = rollArgs ?? {}
-
-  const explode = Math.max(Number(rollArgs?.explode), 1)
 
   const attributeValue = this.attributes[attbName].value + this.attributes[attbName].bonus
   return getAttributeDice(attributeValue + bonus, { advantage, explode }).detailedRoll()
